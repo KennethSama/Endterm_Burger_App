55e9f39b086f12dc50aa6a9fe9658b3207f8c855
kenjie-sama <86190550+kenjie-sama@users.noreply.github.com>
2021-11-23 00:59:16 -0800

    activity_view_product.xml  - Added Name, Price and Ingredients Fields  - Added button that toggles product description visibility

--- app/src/main/java/com/program/endtermburgerapp/view_product.java
+++ app/src/main/java/com/program/endtermburgerapp/view_product.java
@@ -1,14 +1,26 @@
 package com.program.endtermburgerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class view_product extends AppCompatActivity {
-
+    private ConstraintLayout constraintLayout_ProductInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
+        Init_Components();
+    }
+    private void Init_Components(){
+        constraintLayout_ProductInfo = findViewById(R.id.constraintLayout_ProductInfo);
+    }
+    public void HideDescription(View v){
+        constraintLayout_ProductInfo.setVisibility(View.GONE);
+    }
+    public void ShowDescription(View v){
+        constraintLayout_ProductInfo.setVisibility(View.VISIBLE);
     }
 }
