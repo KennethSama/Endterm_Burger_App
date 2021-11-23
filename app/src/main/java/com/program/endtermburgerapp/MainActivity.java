4ba1b957207bf47b1bd7955e9d3e08ebdd3f749a
Rizuku <megaexodiastudios@gmail.com>
2021-11-23 18:55:45 +0800

    Finished code

--- app/src/main/java/com/program/endtermburgerapp/MainActivity.java
+++ app/src/main/java/com/program/endtermburgerapp/MainActivity.java
@@ -6,9 +6,19 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.FocusFinder;
+import android.view.Menu;
 import android.view.View;
 
+import java.util.Formatter;
+
 public class MainActivity extends AppCompatActivity {
+
+    MenuItem ultimeatBurger = new MenuItem();
+    MenuItem kingSize = new MenuItem();
+    MenuItem biteSize = new MenuItem();
+    MenuItem classic = new MenuItem();
+    MenuItem chicken = new MenuItem();
 
 
     @Override
@@ -21,41 +31,57 @@ public class MainActivity extends AppCompatActivity {
 
     void Start(){
 
+        ultimeatBurger.MenuItem("Ultimeat Burger", 139, "Double beef patties with bacon, lettuce, tomato, onions and cheese.", "pikpng_com_burger_png_images_2761101__1_");
+        kingSize.MenuItem("King Size", 169, "A large burger that offers triple beef patties with lettuce, tomato, onion and double cheese", "kingsize_enlarge");
+        biteSize.MenuItem("Bite Size", 99, "Beef patty with lettuce tomato and onion", "bitesize_enlarge");
+        classic.MenuItem("Classic", 119, "Beef patty with lettuce, tomato, pickles, onion, bacon and cheese", "classic_enlarge");
+        chicken.MenuItem("Chicken", 99, "A chicken burger with lettuce, tomato and special sauce", "chicken_enlarge");
+
 
     }
 
     public void onMenuItemClicked(View v){
         if(v.getId() == R.id.menuItem1){
-            LoadDetailedMenuItem(R.id.menuItem1);
+            LoadDetailedMenuItem(ultimeatBurger);
         }
         else if(v.getId() == R.id.menuItem2){
-            LoadDetailedMenuItem(R.id.menuItem2);
-
+            LoadDetailedMenuItem(kingSize);
         }
         else if(v.getId() == R.id.menuItem3){
-
+            LoadDetailedMenuItem(biteSize);
         }
         else if(v.getId() == R.id.menuItem4){
-
+            LoadDetailedMenuItem(classic);
         }
         else if(v.getId() == R.id.menuItem5){
-
+            LoadDetailedMenuItem(chicken);
         }
     }
 
-    void LoadDetailedMenuItem(int menuItem){
+    void LoadDetailedMenuItem(MenuItem menuItem){
+        Intent loadMenuItem = new Intent(MainActivity.this, view_product.class);
 
+        Formatter formatter = new Formatter();
+        formatter.format("%.2f", menuItem.price);
+
+        loadMenuItem.putExtra("ITEM NAME", menuItem.name);
+        loadMenuItem.putExtra("ITEM PRICE", formatter.toString());
+        loadMenuItem.putExtra("ITEM INGREDIENTS", menuItem.ingredients);
+        loadMenuItem.putExtra("ITEM IMAGE", menuItem.imageName);
+        startActivity(loadMenuItem);
     }
 
     public class MenuItem{
         String name = "";
         double price = 0;
         String ingredients = "";
+        String imageName = "";
 
-        public void MenuItem(String _name, double _price, String _ingredients){
+        public void MenuItem(String _name, double _price, String _ingredients, String _imageName){
             name = _name;
             price = _price;
             ingredients = _ingredients;
+            imageName = _imageName;
         }
     }
 }
--- app/src/main/java/com/program/endtermburgerapp/view_product.java
+++ app/src/main/java/com/program/endtermburgerapp/view_product.java
@@ -5,22 +5,42 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class view_product extends AppCompatActivity {
     private ConstraintLayout constraintLayout_ProductInfo;
+
+    TextView tv_itemName;
+    TextView tv_itemPrice;
+    TextView tv_itemIngredients;
+    ImageView image_itemImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
         Init_Components();
+        LoadItemDetails();
     }
     private void Init_Components(){
         constraintLayout_ProductInfo = findViewById(R.id.constraintLayout_ProductInfo);
+        tv_itemName = findViewById(R.id.textView_nameValue);
+        tv_itemPrice = findViewById(R.id.textView_priceValue);
+        tv_itemIngredients = findViewById(R.id.textView_Ingredients);
+        image_itemImage = findViewById(R.id.image_itemImage);
     }
     public void HideDescription(View v){
         constraintLayout_ProductInfo.setVisibility(View.GONE);
     }
     public void ShowDescription(View v){
         constraintLayout_ProductInfo.setVisibility(View.VISIBLE);
+    }
+
+    private void LoadItemDetails(){
+        tv_itemName.setText(getIntent().getStringExtra("ITEM NAME"));
+        tv_itemPrice.setText(getIntent().getStringExtra("ITEM PRICE"));
+        tv_itemIngredients.setText(getIntent().getStringExtra("ITEM INGREDIENTS"));
+        image_itemImage.setImageResource(getResources().getIdentifier(getIntent().getStringExtra("ITEM IMAGE"), "drawable", getPackageName()));
     }
 }
