092ca2bf6659c3d3ff599b8fab6ffb44615ce066
Rizuku <megaexodiastudios@gmail.com>
2021-11-23 14:55:23 +0800

    Added partial code

--- app/src/main/java/com/program/endtermburgerapp/MainActivity.java
+++ app/src/main/java/com/program/endtermburgerapp/MainActivity.java
@@ -3,14 +3,56 @@ package com.program.endtermburgerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Start();
+    }
+
+    void Start(){
+
+
+    }
+
+    public void onMenuItemClicked(View v){
+        if(v.getId() == R.id.menuItem1){
+            LoadDetailedMenuItem(R.id.menuItem1);
+        }
+        else if(v.getId() == R.id.menuItem2){
+
+        }
+        else if(v.getId() == R.id.menuItem3){
+
+        }
+        else if(v.getId() == R.id.menuItem4){
+
+        }
+        else if(v.getId() == R.id.menuItem5){
+
+        }
+    }
+
+    void LoadDetailedMenuItem(int menuItem){
+
+    }
+
+    public class MenuItem{
+        String name = "";
+        double price = 0;
+
+        public void MenuItem(String _name, double _price){
+            name = _name;
+            price = _price;
+        }
     }
 }
